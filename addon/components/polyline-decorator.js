060032b8adc56e4c6af4cca36c0d65b393d0b385
Ubuntu <brad.overton@lsmtechnologies.com.au>
2017-08-05 08:31:16 +0000

    Polyline decorator now updates when supplied patterns object changes

--- addon/components/polyline-decorator.js
+++ addon/components/polyline-decorator.js
@@ -12,5 +12,32 @@ export default BaseLayer.extend({
 
   createLayer() {
     return this.L.polylineDecorator(this.get('requiredOptions'), this.get('options'));
+  },
+
+  didInsertParent() {
+    this._super(...arguments);
+    this._addPatternObserver();
+  },
+
+  willDestroyParent() {
+    this._removePatternObserver();
+    this._super(...arguments);
+  },
+  
+  // Observe for when patterns option changes, so the decorator can be redrawn with the new pattern
+  _addPatternObserver() {
+    this._patternObserver = null;
+    
+    this._patternObserver = function() {
+      let value = this.get('patterns');
+      this._layer.setPatterns(value);
+    };
+
+    this.addObserver('patterns', this, this._patternObserver);
+  },
+
+  _removePatternObserver() {
+    this.removeObserver('patterns', this, this._patternObserver);
+    this._patternObserver = null;
   }
 });
